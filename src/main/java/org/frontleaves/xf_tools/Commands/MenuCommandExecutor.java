d342b2a46a8f817dffd1f1273715377d5cc15e8c
XiaoLFeng <gm@x-lf.cn>
2023-04-28 08:01:54 +0800

    菜单迭代

--- src/main/java/org/frontleaves/xf_tools/Commands/MenuCommandExecutor.java
+++ src/main/java/org/frontleaves/xf_tools/Commands/MenuCommandExecutor.java
@@ -14,46 +14,52 @@ public class MenuCommandExecutor implements CommandExecutor {
         this.Tools = Tools;
     }
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
         if (command.getName().equalsIgnoreCase("xftool")
                 || command.getName().equalsIgnoreCase("xftl")
                 || command.getName().equalsIgnoreCase("xf-tool")
                 || command.getName().equalsIgnoreCase("xf-tl")) {
+            System.out.println();
             if (strings.length == 0) {
-                commandSender.sendMessage(String.format("§7§l§m==]========== %s§7§l§m ==========[==",Tools.XF_Prefix));
-                commandSender.sendMessage(String.format("§3筱锋工具箱[XF_Tool]（版本号：%s）", Tools.XF_Version));
-                commandSender.sendMessage(String.format("§3作者：%s", Tools.XF_Author));
-                commandSender.sendMessage("§3博客：§ehttps://www.blog.x-lf.com/");
-                commandSender.sendMessage("§2输入 §6/xf-tool help §2 查看插件帮助吧");
-                commandSender.sendMessage(String.format("§7§l§m==]========== %s§7§l§m ==========[==",Tools.XF_Prefix));
+                sender.sendMessage(String.format("§7§l§m==]========== %s§7§l§m ==========[==",Tools.XF_Prefix));
+                sender.sendMessage(String.format("§3筱锋工具箱[XF_Tool]（版本号：%s）", Tools.XF_Version));
+                sender.sendMessage(String.format("§3作者：%s", Tools.XF_Author));
+                sender.sendMessage("§3博客：§ehttps://www.blog.x-lf.com/");
+                sender.sendMessage("§2输入 §6/xf-tool help §2 查看插件帮助吧");
+                sender.sendMessage(String.format("§7§l§m==]========== %s§7§l§m ==========[==",Tools.XF_Prefix));
             } else {
                 if (Objects.equals(strings[0], "help")) {
                     if (strings.length == 1) {
-                        commandSender.sendMessage(String.format("§7§l§m==]========== %s§7§l§m ==========[==",Tools.XF_Prefix));
-                        commandSender.sendMessage("§6/xf-tool §7- 查看插件信息");
-                        commandSender.sendMessage("§6/xf-tool help [page] §7- 查看指令帮助");
-                        commandSender.sendMessage("§6/xf-tool setting §7- 插件基础设置");
-                        commandSender.sendMessage(String.format("§7§l§m==]========== %s§7§l§m ==========[==",Tools.XF_Prefix));
-                        commandSender.sendMessage("§a页码： §71§8/§71");
+                        sender.sendMessage(String.format("§7§l§m==]========== %s§7§l§m ==========[==",Tools.XF_Prefix));
+                        sender.sendMessage("§6/xf-tool §7- 查看插件信息");
+                        sender.sendMessage("§6/xf-tool help [page] §7- 查看指令帮助");
+                        sender.sendMessage("§6/xf-op §7- OP管理");
+                        sender.sendMessage("§6/xf-tool setting §7- 插件基础设置");
+                        sender.sendMessage(String.format("§7§l§m==]========== %s§7§l§m ==========[==",Tools.XF_Prefix));
+                        sender.sendMessage("§a页码： §71§8/§71");
                     } else if (strings.length == 2) {
                         if (Objects.equals(strings[1], "1")) {
-                            commandSender.sendMessage(String.format("§7§l§m==]========== %s§7§l§m ==========[==",Tools.XF_Prefix));
-                            commandSender.sendMessage("§6/xf-tool §7- 查看插件信息");
-                            commandSender.sendMessage("§6/xf-tool help [page] §7- 查看指令帮助");
-                            commandSender.sendMessage("§6/xf-tool op §7- 赋予OP操作");
-                            commandSender.sendMessage("§6/xf-tool setting §7- 插件基础设置");
-                            commandSender.sendMessage(String.format("§7§l§m==]========== %s§7§l§m ==========[==",Tools.XF_Prefix));
-                            commandSender.sendMessage("§a页码： §71§8/§71");
-                        } else commandSender.sendMessage(String.format("%s§r%s §r§c页码超出范围！", Tools.XF_Prefix, Tools.XF_PrefixArrow));
+                            sender.sendMessage(String.format("§7§l§m==]========== %s§7§l§m ==========[==",Tools.XF_Prefix));
+                            sender.sendMessage("§6/xf-tool §7- 查看插件信息");
+                            sender.sendMessage("§6/xf-tool help [page] §7- 查看指令帮助");
+                            sender.sendMessage("§6/xf-op §7- OP管理");
+                            sender.sendMessage("§6/xf-tool setting §7- 插件基础设置");
+                            sender.sendMessage(String.format("§7§l§m==]========== %s§7§l§m ==========[==",Tools.XF_Prefix));
+                            sender.sendMessage("§a页码： §71§8/§71");
+                        } else sender.sendMessage(String.format("%s§r%s §r§c页码超出范围！", Tools.XF_Prefix, Tools.XF_PrefixArrow));
                     } else {
-                        commandSender.sendMessage(String.format("%s§r%s §r§c参数不正确，请输入 §6/xf-tool help §c查看指令帮助", Tools.XF_Prefix, Tools.XF_PrefixArrow));
+                        sender.sendMessage(String.format("%s§r%s §r§c参数不正确，请输入 §6/xf-tool help §c查看指令帮助", Tools.XF_Prefix, Tools.XF_PrefixArrow));
                     }
                 } else if (Objects.equals(strings[0], "setting")) {
-                    commandSender.sendMessage(String.format("§7§l§m==]========== %s§7§l§m ==========[==",Tools.XF_Prefix));
-                    commandSender.sendMessage("§6/xf-tool update §7- 查看是否有可选更新");
-                    commandSender.sendMessage("§6/xf-tool version §7- 查看插件版本");
-                    commandSender.sendMessage(String.format("§7§l§m==]========== %s§7§l§m ==========[==",Tools.XF_Prefix));
-                }
+                    sender.sendMessage(String.format("§7§l§m==]========== %s§7§l§m ==========[==",Tools.XF_Prefix));
+                    sender.sendMessage("§6/xf-tool update §7- 查看是否有可选更新");
+                    sender.sendMessage("§6/xf-tool version §7- 查看插件版本");
+                    sender.sendMessage(String.format("§7§l§m==]========== %s§7§l§m ==========[==",Tools.XF_Prefix));
+                } else if (Objects.equals(strings[0], "update"))
+                    sender.sendMessage(String.format("%s§r%s §r§c很抱歉，我还没有做接口哈！", Tools.XF_Prefix, Tools.XF_PrefixArrow));
+                else if (Objects.equals(strings[0],"version"))
+                    sender.sendMessage(String.format("%s§r%s §r§3版本号：%s", Tools.XF_Prefix, Tools.XF_PrefixArrow,Tools.XF_Version));
+                else sender.sendMessage(String.format("%s§r%s §r§c参数错误请输入 §6/xf-tool help §c查看帮助", Tools.XF_Prefix, Tools.XF_PrefixArrow));
             }
         }
         return true;
